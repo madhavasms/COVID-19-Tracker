8b0cf810864ce5c6a85f26f6836f0ee9b65dba16
madhavasms <madhavasms@gmail.com>
2020-08-01 00:26:22 +0530

    Updates

---
+++ precache-manifest.5204559f3c755dcec893a1d7b926b02a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "782cc38c19f30f792567221933be5715",
+    "url": "/COVID-19-Tracker/index.html"
+  },
+  {
+    "revision": "05db2cfe5c07dfa6d339",
+    "url": "/COVID-19-Tracker/static/css/main.2a54473c.chunk.css"
+  },
+  {
+    "revision": "04639268d9f7012d6804",
+    "url": "/COVID-19-Tracker/static/js/2.be8c802e.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID-19-Tracker/static/js/2.be8c802e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05db2cfe5c07dfa6d339",
+    "url": "/COVID-19-Tracker/static/js/main.7ce7f960.chunk.js"
+  },
+  {
+    "revision": "c95c92c1915eb4657fd7",
+    "url": "/COVID-19-Tracker/static/js/runtime-main.9cc3d6ca.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVID-19-Tracker/static/media/image.d7265326.png"
+  }
+]);
